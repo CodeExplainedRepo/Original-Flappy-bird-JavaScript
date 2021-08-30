5ac72f1e4d8854e7f79f38ddd38fbd97bef77320
WMYM <81931984+WashingtonYandun@users.noreply.github.com>
2021-08-30 14:42:13 -0500

    Description
    Description

---
+++ onesAndZeros.py
@@ -0,0 +1,11 @@
+'''
+ONES AND ZEROS
+
+from Codewars by user4386369
+
+Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+Examples:
+'''
